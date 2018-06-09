a12bbb68bfa635b003332637a25788a981c62015
Carlos Mafla <gigo6000@hotmail.com>
2018-06-09 11:17:04 -0500

    Clean up example cookbook

--- chef/cookbooks/example/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-# This is a Chef recipe file. It can be used to specify resources which will
-# apply configuration to a server.
-
-log "Welcome to Chef, #{node['example']['name']}!" do
-  level :info
-end
-
-# For more information, see the documentation: https://docs.chef.io/recipes.html
